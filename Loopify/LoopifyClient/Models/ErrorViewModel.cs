3768b70b8a42f5033a14bf309e8a712773af26e1
HaLBH <144716364+HaLBH176637@users.noreply.github.com>
2024-10-10 14:58:12 +0700

    add db

---
+++ Loopify/LoopifyClient/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoopifyClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
